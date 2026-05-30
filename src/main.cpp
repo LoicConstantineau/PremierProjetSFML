ff431484c276beff8dc851954663b80f0f4f976e
LoicConstantineau <locon7@ulaval.ca>
2026-05-30 11:05:44 -0400

    Setup de départ

---
+++ src/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <myHeader.h>
+#include <fstream>
+
+#include <headers/header.h>
+
+int main()
+{
+	
+	print();
+
+
+	std::ifstream file(RESOURCES_PATH "file.txt");
+	
+	//read the file
+	std::string str((std::istreambuf_iterator<char>(file)),
+		std::istreambuf_iterator<char>());
+	std::cout << str;
+
+
+	std::cin.get();
+	return 0;
+}
+
+
