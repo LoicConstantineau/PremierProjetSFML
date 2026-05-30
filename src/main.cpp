4c2e85e473dde336f2c2b28d7a747e257dda4dfe
LoicConstantineau <locon7@ulaval.ca>
2026-05-30 14:32:48 -0400

    Tete de faite

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,6 @@
 #include <SFML/Graphics.hpp>
 
 
-
 int main()
 {
 	unsigned int largeurFenetre = 1200;
@@ -10,6 +9,22 @@ int main()
 
 	sf::RenderWindow window(sf::VideoMode({largeurFenetre, hauteurFenetre}), "Cercle");
  	window.setFramerateLimit(60);
+
+	sf::ConvexShape tete(8);
+	tete.setPoint(0, sf::Vector2f(10, 10));
+	tete.setPoint(1, sf::Vector2f(60, 10));
+	tete.setPoint(2, sf::Vector2f(60, 60));
+	tete.setPoint(3, sf::Vector2f(45, 60));
+	tete.setPoint(4, sf::Vector2f(45, 80));
+	tete.setPoint(5, sf::Vector2f(25, 80));
+	tete.setPoint(6, sf::Vector2f(25, 60));
+	tete.setPoint(7, sf::Vector2f(10, 60));
+	tete.setFillColor(sf::Color::White);
+	tete.setPosition({10, 10});
+
+	sf::ConvexShape corps(4);
+	sf::ConvexShape bras(4);
+	sf::ConvexShape jambe(4);
 
 	while (window.isOpen()){
 
@@ -19,6 +34,7 @@ int main()
       		}
 		}
 		window.clear();
+		window.draw(tete);
 		window.display();
 	}
 	return 0;
